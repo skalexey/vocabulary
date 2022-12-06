bb95d2b7c77f00c4eb67cf6fc7598dda1e776b9a
skalexey <skorohodov-ad@narod.ru>
2022-12-06 21:31:27 +0100

    Support imgui and SDL crossplatform build

--- deps_scenario.sh
+++ deps_scenario.sh
@@ -7,6 +7,12 @@ function deps_scenario()
 
     download_dependency "VL" "$depsLocation" "git@github.com:skalexey/VL.git"
     download_dependency "lua-5.4.4" "$depsLocation" "http://www.lua.org/ftp/lua-5.4.4.tar.gz"
+    download_dependency "imgui" "$depsLocation" "https://github.com/ocornut/imgui.git"
+    download_dependency "SDL" "$depsLocation" "https://github.com/libsdl-org/SDL.git"
+
+    if [ ! -f "main_sdl_imgui.cpp" ]; then
+        cp "$depsLocation/imgui/examples/example_sdl_sdlrenderer/main.cpp" "main_sdl_imgui.cpp"
+    fi
 }
 
 deps_scenario $@
