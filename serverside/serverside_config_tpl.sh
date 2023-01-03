b251358c1fc9fe02dbe6423cd7b065e02be5ef22
skalexey <skorohodov-ad@narod.ru>
2023-01-03 15:01:58 +0100

    serverside_config_tpl.sh

---
+++ serverside/serverside_config_tpl.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+domain_dir="domains/my.domain.com"
+project_dir_name=""
+ssh_host="my.domain.com"
+ssh_port=22
+ssh_user=""
+ssh_pass=""
+
+remote_dir="$domain_dir/$project_dir_name"
