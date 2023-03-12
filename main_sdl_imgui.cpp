c28a3b9e86296eb003158843a3960fe7c543dad2
skalexey <skorohodov-ad@narod.ru>
2023-03-12 20:14:15 +0100

    Fix wrong behaviour on Android.

--- main_sdl_imgui.cpp
+++ main_sdl_imgui.cpp
@@ -1,12 +1,5 @@
-// Dear ImGui: standalone example application for SDL2 + SDL_Renderer
-// (SDL is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
-// Read online: https://github.com/ocornut/imgui/tree/master/docs
-
-// Important to understand: SDL_Renderer is an _optional_ component of SDL. We do not recommend you use SDL_Renderer
-// because it provide a rather limited API to the end-user. We provide this backend for the sake of completeness.
-// For a multi-platform app consider using e.g. SDL+DirectX on Windows and SDL+OpenGL on Linux/OSX.
-
+// TODO: figure out why SDL.h include is needed on Android (it just a stuck without it)
+#include <SDL.h>
 #include "app.h"
 
 app* g_app = nullptr;
