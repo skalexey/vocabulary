616d19caec88817f91bd4dba2dd76d17d779043a
Alex <skorohodov-ad@narod.ru>
2024-01-16 09:04:07 +0100

    Networking/http dependency.

--- deps_scenario.sh
+++ deps_scenario.sh
@@ -12,7 +12,7 @@ function deps_scenario()
     source "$depsLocation/DataModelBuilder/Core/deps_scenario.sh"
     download_dependency "DataModelBuilder" "$depsLocation" "git@github.com:skalexey/DataModelBuilder.git"
     download_dependency "Networking" "$depsLocation" "git@github.com:skalexey/Networking.git"
-    source "$depsLocation/Networking/deps_scenario.sh"
+    source "$depsLocation/Networking/http/deps_scenario.sh"
     download_dependency "lua-5.4.4" "$depsLocation" "http://www.lua.org/ftp/lua-5.4.4.tar.gz"
     download_dependency "boost_1_81_0" "$depsLocation" "https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.bz2"
     download_dependency "abstract_ui" "$depsLocation" "git@github.com:skalexey/abstract_ui.git"
